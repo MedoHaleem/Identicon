ea315503329af36ef4f3f7c75534fba6118f7d1f
Medo <supermedo.sams@gmail.com>
2017-10-27 23:13:47 +0200

    Create Grid Filter the Grid for even numbers

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,37 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+  end
+
+  def build_grid(image) do
+    grid =
+      image.hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    Enum.filter grid, fn ({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
   def pick_color(image) do
     [r, g, b | _tail] = image.hex
