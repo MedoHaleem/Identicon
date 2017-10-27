e6116f8bd3d1ec24a8edd5d865a9cfa6603af4e9
Medo <supermedo.sams@gmail.com>
2017-10-27 19:23:41 +0200

    Initial commit

---
+++ lib/identicon.ex
@@ -0,0 +1,18 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+  end
+  def pick_color(image) do
+    [r, g, b | _tail] = image.hex
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+          |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
